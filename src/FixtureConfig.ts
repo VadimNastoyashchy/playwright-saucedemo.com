36b6ae82ec9481f40eb74be2a3af317b95e71b4d
vadym_nastoiashchyi <vadym_nastoiashchyi@epam.com>
2024-01-13 20:57:18 +0200

    Update

---
+++ src/FixtureConfig.ts
@@ -0,0 +1,27 @@
+import { test as base } from '@playwright/test';
+import LoginPage from './pages/LoginPage';
+import InventoryPage from './pages/InventoryPage';
+import ApiService from './ApiService';
+
+type MyFixtures = {
+  loginPage: LoginPage;
+  inventoryPage: InventoryPage;
+  apiService: ApiService;
+};
+
+export const test = base.extend<MyFixtures>({
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await use(loginPage);
+  },
+  inventoryPage: async ({ page }, use) => {
+    const inventoryPage = new InventoryPage(page);
+    await use(inventoryPage);
+  },
+  apiService: async ({ page }, use) => {
+    const apiService = new ApiService(page);
+    await use(apiService);
+  },
+
+});
+export { expect } from '@playwright/test';
--- tests/logIn.spec.ts
+++ tests/logIn.spec.ts
@@ -1,44 +1,30 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from '../src/FixtureConfig';
 import { AccountType } from '../src/AccountType';
-import ApiService from '../src/ApiService';
 import Credentials from '../src/Credentials';
-import InventoryPage from '../src/pages/InventoryPage';
-import LoginPage from '../src/pages/LoginPage';
 
 test.describe('Login and Logout test', () => {
-    test('Login with \'standard\' user', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        const inventoryPage = new InventoryPage(page);
-
+    test('Login with \'standard\' user', async ({ page, loginPage, inventoryPage }) => {
         await loginPage.open();
         await expect(await page.url()).toContain(await loginPage.getPageUrl());
         await loginPage.logInWithCredentials(Credentials.getUserCredentials(AccountType.Standard));
         await expect(await page.url()).toContain(await inventoryPage.getPageUrl());
     });
 
-    test('Login with \'problem\' user', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        const inventoryPage = new InventoryPage(page);
-
+    test('Login with \'problem\' user', async ({ page, loginPage, inventoryPage }) => {
         await loginPage.open();
         await expect(await page.url()).toContain(await loginPage.getPageUrl());
         await loginPage.logInWithCredentials(Credentials.getUserCredentials(AccountType.Problem));
         await expect(await page.url()).toContain(await inventoryPage.getPageUrl());
     });
 
-    test('Login with \'locked\' user', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-
+    test('Login with \'locked\' user', async ({ page, loginPage }) => {
         await loginPage.open();
         await expect(await page.url()).toContain(await loginPage.getPageUrl());
         await loginPage.logInWithCredentials(Credentials.getUserCredentials(AccountType.LocKed));
         await expect(await loginPage.getErrorMessage()).toContain('Epic sadface: Sorry, this user has been locked out.');
     });
 
-    test('Login with \'standard\' user and log out of the account to check that the fields are empty', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        const inventoryPage = new InventoryPage(page);
-
+    test('Login with \'standard\' user and log out of the account to check that the fields are empty', async ({ page, loginPage, inventoryPage }) => {
         await loginPage.open();
         await expect(await page.url()).toContain(await loginPage.getPageUrl());
         await loginPage.logInWithCredentials(Credentials.getUserCredentials(AccountType.Standard));
@@ -49,10 +35,7 @@ test.describe('Login and Logout test', () => {
         await expect(await loginPage.inputPasswordField).toHaveValue('');
     });
 
-    test('Login with \'standard\' user with set cookies', async ({ page }) => {
-        const apiService = new ApiService(page);
-        const inventoryPage = new InventoryPage(page);
-
+    test('Login with \'standard\' user with set cookies', async ({ page, apiService, inventoryPage }) => {
         await apiService.logIn(Credentials.getUserCredentials(AccountType.Standard));
         await inventoryPage.open();
         await expect(await page.url()).toContain(await inventoryPage.getPageUrl());
